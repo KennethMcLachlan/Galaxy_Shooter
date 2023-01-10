1802f1537308957f353a3704a56201d613dec0f0
KennethMcLachlan <kennethmclachlan11@gmail.com>
2023-01-10 10:16:44 -0800

    First Commit

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Take the current position = new position (0, 0, 0)
+        transform.position = new Vector3(0, 0, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector3.right * 5 * Time.deltaTime);
+                        // new Vector3(1, 0, 0)
+    }
+}
